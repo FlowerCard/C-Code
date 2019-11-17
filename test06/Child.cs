609cf30c2d5f80a74e427d53cafa23875ffe3ca2
FlowerCard <40333039+FlowerCard@users.noreply.github.com>
2019-11-17 10:42:23 +0800

    添加项目文件。

---
+++ test06/Child.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test06
+{
+    class Child
+    {
+        private string name;
+        private string sex;
+        private int age;
+        private int height;
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public string Sex
+        {
+            get
+            {
+                return sex;
+            }
+
+            set
+            {
+                sex = value;
+            }
+        }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        /// <summary>
+        /// 身高
+        /// </summary>
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                height = value;
+            }
+        }
+
+        /// <summary>
+        /// 求和
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public double Sum(double x,double y)
+        {
+            double sum = x + y;
+            return sum;
+        }
+
+        public Child()
+        {
+            Name = "周星星";
+        }
+    }
+}
---
+++ test06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child child = new Child();//构造方法的调用
+            Console.WriteLine("我的名字是{0}", child.Name);
+            //double sum;
+            //sum = child.Sum(3, 4);
+            //Console.WriteLine("和为{0}", sum);
+        }
+    }
+}
+
+//构造方法的作用：为属性赋值
+//如果没有显示定义构造方法，则会有一个默认的无参数的构造方法
+//如果显示定义了构造方法，则没有默认构造方法
+//只能用new 方法名()的形式调用构造方法
+//构造方法通常声明称public
+//构造方法没有返回值类型
+//构造方法名与类名相同

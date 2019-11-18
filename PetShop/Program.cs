609118221b0848c8e435617e40af2205ca0f85bd
FlowerCard <1274026997@qq.com>
2019-11-18 20:45:07 +0800

    宠物商店

---
+++ PetShop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop
+{
+
+    public class Pet
+    {
+        public string Name;
+        public void PrintName()
+        {
+            Console.WriteLine("Pet`s name is " + Name);
+        }
+    }
+
+    public class Dog : Pet
+    {
+
+    }
+
+    public class Cat : Pet
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Name = "Jack";
+            dog.PrintName();
+
+            Cat cat = new Cat();
+            cat.Name = "Tom";
+            cat.PrintName();
+        }
+    }
+}

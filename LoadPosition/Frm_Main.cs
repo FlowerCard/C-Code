9f64426768d070b094b33a6fc1ecd95b8d08c952
FlowerCard <1274026997@qq.com>
2019-11-19 18:56:33 +0800

    LoadPosition

---
+++ LoadPosition/Frm_Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoadPosition
+{
+    public partial class Frm_Main : Form
+    {
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Main_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.CenterParent;//设置窗体居中
+        }
+    }
+}

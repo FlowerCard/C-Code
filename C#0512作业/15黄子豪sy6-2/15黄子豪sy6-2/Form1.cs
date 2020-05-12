2ffae7d9d4d49da5ff5b1f8bae107be44c9a4755
FlowerCard <1274026997@qq.com>
2020-05-12 21:03:11 +0800

    模拟用户登录

---
+++ C#0512作业/15黄子豪sy6-2/15黄子豪sy6-2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace _15黄子豪sy6_2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sql = new SqlConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+            sql.ConnectionString = "server=.;Data Source=DESKTOP-VSKS0LK;Initial Catalog=student_course;Persist Security Info=True;User ID=sa;Password=SIhunmie27.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string UserName = textBox1.Text;
+            string PassWord = textBox2.Text;
+            string sqlUserName = "select id from admin where id = '" + UserName + "'";
+            SqlCommand cmd01 = new SqlCommand(sqlUserName, sql);
+            string sqlPassWord = "select psw from admin where id='" + UserName + "'";
+            SqlCommand cmd02 = new SqlCommand(sqlPassWord, sql);
+            sql.Open();
+            string reUserName = cmd01.ExecuteScalar().ToString();
+            string rePassWord = cmd02.ExecuteScalar().ToString();
+            cmd02.ExecuteNonQuery();
+            sql.Close();
+
+            if(UserName!=reUserName || PassWord != rePassWord)
+            {
+                MessageBox.Show("账号或密码错误", "提示");
+            }
+            else
+            {
+                MessageBox.Show("登录成功", "提示");
+            }
+        }
+    }
+}

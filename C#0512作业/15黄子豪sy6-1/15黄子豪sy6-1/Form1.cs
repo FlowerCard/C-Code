8e3fd391fc855de08e70ae5d04986350d222b4d3
FlowerCard <1274026997@qq.com>
2020-05-12 19:03:48 +0800

    SqlServer数据库连接

---
+++ C#0512作业/15黄子豪sy6-1/15黄子豪sy6-1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _15黄子豪sy6_1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sql = new SqlConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+            sql.ConnectionString = "server=.;Data Source=DESKTOP-VSKS0LK;Initial Catalog=student_course;Persist Security Info=True;User ID=sa;Password=SIhunmie27.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sql.Open();
+            label2.Text = sql.State.ToString();
+            sql.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sql.Close();
+            label2.Text = sql.State.ToString();
+        }
+    }
+}

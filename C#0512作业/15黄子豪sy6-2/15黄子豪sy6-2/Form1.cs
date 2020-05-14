a0c7c14fece08ea4ea5f890754c404bc1b2a1af9
FlowerCard <40333039+FlowerCard@users.noreply.github.com>
2020-05-14 11:03:57 +0800

    Update Form1.cs

--- C#0512作业/15黄子豪sy6-2/15黄子豪sy6-2/Form1.cs
+++ C#0512作业/15黄子豪sy6-2/15黄子豪sy6-2/Form1.cs
@@ -25,16 +25,23 @@ namespace _15黄子豪sy6_2
         {
             string UserName = textBox1.Text;
             string PassWord = textBox2.Text;
-            string sqlUserName = "select id from admin where id = '" + UserName + "'";
-            SqlCommand cmd01 = new SqlCommand(sqlUserName, sql);
-            string sqlPassWord = "select psw from admin where id='" + UserName + "'";
-            SqlCommand cmd02 = new SqlCommand(sqlPassWord, sql);
-            sql.Open();
-            string reUserName = cmd01.ExecuteScalar().ToString();
-            string rePassWord = cmd02.ExecuteScalar().ToString();
-            cmd02.ExecuteNonQuery();
-            sql.Close();
-
+            string reUserName = null;
+            string rePassWord = null;
+            try
+            {
+                string sqlUserName = "select id from admin where id = '" + UserName + "'";
+                SqlCommand cmd01 = new SqlCommand(sqlUserName, sql);
+                string sqlPassWord = "select psw from admin where id='" + UserName + "'";
+                SqlCommand cmd02 = new SqlCommand(sqlPassWord, sql);
+                sql.Open();
+                string reUserName = cmd01.ExecuteScalar().ToString();
+                string rePassWord = cmd02.ExecuteScalar().ToString();
+                sql.Close();
+            }
+            catch
+            {
+            }
+               
             if (UserName != reUserName || PassWord != rePassWord)
             {
                 MessageBox.Show("账号或密码错误", "提示");
